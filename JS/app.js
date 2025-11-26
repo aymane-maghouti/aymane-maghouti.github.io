a10d731c47a522c2f50b2e64b968c0f433b1051d
aymane-maghouti <aymanemaghouti@gmail.com>
2025-11-26 23:36:18 +0100

    V2

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,7 @@
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
-const header = document.querySelector('.header.container');
+const header = document.querySelector('#header');
 
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
@@ -10,10 +10,10 @@ hamburger.addEventListener('click', () => {
 
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
-	if (scroll_position > 250) {
-		header.style.backgroundColor = '#29323c';
+	if (scroll_position > 100) {
+		header.classList.add('scrolled');
 	} else {
-		header.style.backgroundColor = 'transparent';
+		header.classList.remove('scrolled');
 	}
 });
 
